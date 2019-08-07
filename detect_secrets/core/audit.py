6984b110ee1485088ff6dbb84d93bfe6e6d48957
Justin Eyster <Justin.Eyster@ibm.com>
2019-08-07 10:22:00 -0400

    Wording updates

--- detect_secrets/core/audit.py
+++ detect_secrets/core/audit.py
@@ -28,8 +28,8 @@ from .potential_secret import PotentialSecret
 class SecretNotFoundOnSpecifiedLineError(Exception):
     def __init__(self, line):
         super(SecretNotFoundOnSpecifiedLineError, self).__init__(
-            'ERROR: Secret not found on line {}!\n'.format(line) +
-            'Try recreating your baseline to fix this issue.',
+            'ERROR: Secret not found on line {}!\n'.format(line)
+            + 'Try recreating or updating your baseline to fix this issue.',
         )
 
 
