dc723ab297d09330d0f5ca493b71eb71db2ee882
Justin D. Eyster <Justin.Eyster@ibm.com>
2019-09-09 16:45:10 -0400

    Verification for Artifactory tokens (#190)
    * Verification for Artifactory tokens
    
    Supports git-defenders/detect-secrets-discuss#173
    
    * Remove 443
    
    * Extract artifactory url
    
    * Address @xianjun comments

--- detect_secrets/plugins/artifactory.py
+++ detect_secrets/plugins/artifactory.py
@@ -2,7 +2,10 @@ from __future__ import absolute_import
 
 import re
 
+import requests
+
 from .base import RegexBasedDetector
+from detect_secrets.core.constants import VerifiedResult
 
 
 class ArtifactoryDetector(RegexBasedDetector):
@@ -15,3 +18,23 @@ class ArtifactoryDetector(RegexBasedDetector):
         # Artifactory encrypted passwords begin with AP[A-Z]
         re.compile(r'(?:\s|=|:|"|^)AP[\dABCDEF][a-zA-Z0-9]{8,}'),  # Password
     ]
+
+    artifactory_url = 'na.artifactory.swg-devops.com/artifactory'
+
+    def verify(self, token, **kwargs):
+        try:
+            if type(token) == bytes:
+                token = token.decode('UTF-8')
+            headers = {'X-JFrog-Art-API': token}
+            response = requests.get(
+                'https://%s/api/system/ping' % self.artifactory_url,
+                headers=headers,
+            )
+            if response.status_code == 200:
+                return VerifiedResult.VERIFIED_TRUE
+            elif response.status_code == 401:
+                return VerifiedResult.VERIFIED_FALSE
+            else:
+                return VerifiedResult.UNVERIFIED
+        except Exception:
+            return VerifiedResult.UNVERIFIED
