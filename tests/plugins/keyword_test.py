b787b14f7046b12e2c7a3d98110d3077debde7c3
XIANJUN ZHU <xianjun@us.ibm.com>
2020-05-26 21:47:07 -0400

    Properly print out exclude keyword (#315)
    * Properly print out exclude keyword
    
    * Add tests
    
    * Format
    
    * One more test case for regex
    
    * Fix build

--- tests/plugins/keyword_test.py
+++ tests/plugins/keyword_test.py
@@ -1,6 +1,8 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
+import json
+
 import ahocorasick
 import pytest
 
@@ -405,3 +407,32 @@ class TestKeywordDetector(object):
             'mock_filename.example',
         )
         assert len(output) == 0
+
+    @pytest.mark.parametrize(
+        'keyword_exclude, dict_content',
+        (
+            (
+                None,
+                {'keyword_exclude': None, 'name': 'KeywordDetector'},
+            ),
+            (
+                'keyword',
+                {'keyword_exclude': 'keyword', 'name': 'KeywordDetector'},
+            ),
+            (
+                'a.*|b.*',
+                {'keyword_exclude': 'a.*|b.*', 'name': 'KeywordDetector'},
+            ),
+        ),
+    )
+    def test_dict_output(self, keyword_exclude, dict_content):
+        detector = KeywordDetector(keyword_exclude)
+        actual = json.dumps(
+            detector.__dict__,
+            sort_keys=True,
+        )
+        expected = json.dumps(
+            dict_content,
+            sort_keys=True,
+        )
+        assert actual == expected
